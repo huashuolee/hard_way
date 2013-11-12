93f84c95a831e39b9cc7459642491ca420176a2c
Huashuo Lee <huashuolee@gmail.com>
2013-11-12 10:24:18 +0800

    add ex1~9

---
+++ ex3.py
@@ -0,0 +1,25 @@
+#outout a line
+print "I will now count my chickens:"
+#output the hens number.
+print "Hens", 25 +30/6
+#output the roosters number.
+print "Roosters", 100-25*3%4
+#output a line to count eggs
+print "Now I will count the eggs:"
+#calculate the eggs nubmer
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#true or false?
+print "Is it true that 3+2 <5 -7?"
+#output true or false?
+print 3 + 2 <5 - 7
+#calculate 3+2, 5-7
+print "what is 3+2?", 3+2
+print "what is 5-7?", 5-7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5<= -2
