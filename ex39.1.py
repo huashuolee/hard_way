ddf1dc13d1d110b5d5af0e7e0646cf7d3ba5bf07
Huashuo Lee <huashuolee@gmail.com>
2013-11-28 19:30:31 +0800

    1

---
+++ ex39.1.py
@@ -0,0 +1,13 @@
+states = {
+    'Oregon' : 'OR',
+    'Florida' : 'FL',
+    'California' : 'CA',
+    'New York' : 'NY',
+    'Michigan' : 'MI'
+}
+
+print states['Oregon']
+
+a = ('test', 'good', 'bad')
+
+print a[1]
